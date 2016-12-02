ed65526aa365d7c887692a3843148c36b33a57c8
purehero <purehero@inka.co.kr>
2016-12-02 18:43:03 +0900

    no message

--- MyAndroidSampleApp00/jni/sample00.cpp
+++ MyAndroidSampleApp00/jni/sample00.cpp
@@ -3,13 +3,72 @@
 #include <stdlib.h>
 #include <android/log.h>
 
-#define  LOG_TAG	"Sample00"
+#define  LOG_TAG	"TEST_Native"
 #define  LOGV(...)  __android_log_print(ANDROID_LOG_VERBOSE,LOG_TAG,__VA_ARGS__)
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 #define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
 #define  LOGT()		__android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,"%s:%s",__FILE__,__func__ )
+
+void init( JNIEnv * env, jobject, jobject appContext )
+{
+	LOGT();
+
+	jclass jc_appContext = env->GetObjectClass(appContext);
+	if(jc_appContext == NULL) {
+		LOGE("ERROR: failed to get appContext");
+		return;
+	} else {
+		LOGD("SUCCESS: get appContext");
+	}
+
+	jmethodID jm_getApplicationInfo = env->GetMethodID( jc_appContext, "getApplicationInfo", "()Landroid/content/pm/ApplicationInfo;");
+	if( jm_getApplicationInfo == NULL) {
+		LOGE("ERROR: failed to get getApplicationInfo method");
+		return;
+	} else {
+		LOGD("SUCCESS: get getApplicationInfo method");
+	}
+
+	jobject jo_applicationInfo = ( jclass ) env->CallObjectMethod( appContext, jm_getApplicationInfo );
+	if( jo_applicationInfo == NULL ) {
+		LOGE("ERROR: failed to get getApplicationInfo object");
+		return;
+	} else {
+		LOGD("SUCCESS: get getApplicationInfo object");
+	}
+
+	jclass jc_applicationInfo = env->FindClass( "android/content/pm/ApplicationInfo");
+	if( jc_applicationInfo == NULL ) {
+		LOGE("ERROR: failed to get getApplicationInfo instance");
+		return;
+	} else {
+		LOGD("SUCCESS: get getApplicationInfo instance");
+	}
+
+	jfieldID jf_nativeLibraryDir = env->GetFieldID( jc_applicationInfo, "nativeLibraryDir", "Ljava/lang/String;");
+	if( jf_nativeLibraryDir == NULL ) {
+		LOGE("ERROR: failed to get nativeLibraryDir field");
+		return;
+	} else {
+		LOGD("SUCCESS: get nativeLibraryDir field");
+	}
+
+	jstring nativeLibraryDir = (jstring) env->GetObjectField( jo_applicationInfo, jf_nativeLibraryDir );
+	if( nativeLibraryDir == NULL ) {
+		LOGE("ERROR: failed to get nativeLibraryDir field value");
+		return;
+	} else {
+		LOGD("SUCCESS: et nativeLibraryDir field value");
+	}
+
+
+	char * strNativeLibraryDir = (char*) env->GetStringUTFChars( nativeLibraryDir, 0 );
+	LOGD( "strNativeLibraryDir : %s", strNativeLibraryDir );
+
+	env->ReleaseStringUTFChars( nativeLibraryDir, strNativeLibraryDir );
+}
 
 // JNI_OnLoad
 jint JNI_OnLoad( JavaVM* vm, void* )
@@ -24,7 +83,7 @@ jint JNI_OnLoad( JavaVM* vm, void* )
 		LOGE("ERROR: GetEnv failed");
 		return result;
 	}
-#if 0
+#if 1
 	const char * classPath = "com/example/myandroidsampleapp00/NativeLibrary";
 	jclass clazz = env->FindClass( classPath );
 	if (clazz == NULL)
@@ -34,11 +93,10 @@ jint JNI_OnLoad( JavaVM* vm, void* )
 	}
 
 	JNINativeMethod gMethods[4] = {
-			{ "nCreateRenderer", 	"()I", 		(void*) android_view_GLES20Canvas_createRenderer },
-			{ "nDrawCircle", 		"(IFFFI)V", (void*) android_view_GLES20Canvas_drawCircle }
+			{ "init", 	"(Landroid/content/Context;)V", 		(void*) init }
 	};
 
-	if (env->RegisterNatives( clazz, gMethods, 2 ) < 0 )
+	if (env->RegisterNatives( clazz, gMethods, 1 ) < 0 )
 	{
 		LOGE( "RegisterNatives failed for '%s'", classPath );
 		return -1;
