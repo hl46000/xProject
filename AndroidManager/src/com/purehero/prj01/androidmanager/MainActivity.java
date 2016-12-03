bb68e2861fc2e757ec02c127d6e24509bc645f54
purehero <purehero@gmail.com>
2016-12-04 00:18:53 +0900

    no message

---
+++ AndroidManager/src/com/purehero/prj01/androidmanager/MainActivity.java
@@ -0,0 +1,111 @@
+package com.purehero.prj01.androidmanager;
+
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends ActionBarActivity implements MaterialTabListener {
+	
+	MaterialTabHost tabHost;
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		Toolbar toolbar = (android.support.v7.widget.Toolbar) this.findViewById(R.id.toolbar);
+        this.setSupportActionBar(toolbar);
+
+        tabHost = (MaterialTabHost) this.findViewById(R.id.tabHost);
+        pager = (ViewPager) this.findViewById(R.id.pager );
+        
+     // init view pager
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                // when user do a swipe the selected tab change
+                tabHost.setSelectedNavigationItem(position);
+
+            }
+        });
+
+        // insert all tabs from pagerAdapter data
+        for (int i = 0; i < adapter.getCount(); i++) {
+            tabHost.addTab(
+                    tabHost.newTab()
+                            .setText(adapter.getPageTitle(i))
+                            .setTabListener(this)
+            );
+
+        }
+	}
+
+	/*
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			startActivity( new Intent( this, SettingsActivity.class ) );
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+*/
+	@Override
+	public void onTabSelected(MaterialTab tab) {
+		pager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabReselected(MaterialTab tab) {		
+	}
+
+	@Override
+	public void onTabUnselected(MaterialTab tab) {
+	}
+	
+	
+	private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+
+        }
+
+        public Fragment getItem(int num) {
+            return new Fragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Sezione " + position;
+        }
+
+    }
+}
---
+++ AndroidManager/src/com/purehero/prj01/androidmanager/SettingsActivity.java
@@ -0,0 +1,28 @@
+package com.purehero.prj01.androidmanager;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+public class SettingsActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+				
+		getFragmentManager()
+			.beginTransaction()
+			.replace(android.R.id.content,
+            new MyPreferenceFragment()).commit();
+	}
+
+	// PreferenceFragment ?Å¥?ûò?ä§ ?Ç¨?ö©
+    public static class MyPreferenceFragment extends
+            PreferenceFragment {
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_settings);
+        }
+    }
+}
