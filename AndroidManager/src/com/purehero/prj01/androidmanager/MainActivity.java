e9b8d4483beb66a5e60d9e2ef922f84e3bc53f0c
purehero <purehero@gmail.com>
2016-12-10 00:21:40 +0900

    no message

--- AndroidManager/src/com/purehero/prj01/androidmanager/MainActivity.java
+++ AndroidManager/src/com/purehero/prj01/androidmanager/MainActivity.java
@@ -11,7 +11,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
