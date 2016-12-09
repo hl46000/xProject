95cf16457276495906e95ad1df04f085f81e1ae1
purehero <purehero@gmail.com>
2016-12-10 01:05:38 +0900

    no message

--- AndroidManager/src/com/purehero/prj01/androidmanager/MainActivity.java
+++ AndroidManager/src/com/purehero/prj01/androidmanager/MainActivity.java
@@ -4,13 +4,14 @@ import java.io.File;
 import java.util.Stack;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -21,7 +22,7 @@ import android.widget.Toast;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
-public class MainActivity extends Activity 
+public class MainActivity extends ActionBarActivity 
 {
 	private ListView apkListView 			= null;
 	private ProgressBar progressBar  		= null;
@@ -41,6 +42,24 @@ public class MainActivity extends Activity
 		mAdView.loadAd(adRequest);
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 	
 	
 	@Override
