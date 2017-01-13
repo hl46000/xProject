a1f8a1cc27e34e1fa9d8111eab818df29cbd5f26
purehero <purehero@gmail.com>
2017-01-14 01:01:06 +0900

    no message

--- AndroidQReader/src/com/purehero/qr/reader/MainActivity.java
+++ AndroidQReader/src/com/purehero/qr/reader/MainActivity.java
@@ -1,56 +1,23 @@
 package com.purehero.qr.reader;
 
-import it.neokree.materialtabs.MaterialTab;
-import it.neokree.materialtabs.MaterialTabHost;
-import it.neokree.materialtabs.MaterialTabListener;
-import android.app.ActionBar;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity implements MaterialTabListener {
+import com.google.zxing.client.android.ViewfinderView;
 
-	private MaterialTabHost tabHost;
-	private ViewPager pager;
-    private ViewPagerAdapter adapter;
+public class MainActivity extends com.google.zxing.client.android.CaptureActivity {
     
     @Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		tabHost = (MaterialTabHost) this.findViewById(R.id.tabHost);
-        pager = (ViewPager) this.findViewById(R.id.pager );
-        
-        adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        addTabs( adapter );
-        
-        pager.setAdapter(adapter);
-        pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                tabHost.setSelectedNavigationItem(position);
-            }
-        });
-        
-        for (int i = 0; i < adapter.getCount(); i++) {
-            tabHost.addTab( tabHost.newTab().setText(adapter.getPageTitle(i)).setTabListener(this));
-        }
-	}
-
-	/**
-	 * Tab에 들어갈 Fragment 을 생성하여 adapter에 등록한다. 
-	 * 
-	 * @param adapter
-	 */
-	private void addTabs( ViewPagerAdapter adapter ) {
-		adapter.addFragment( new FragmentText("테스트 1"), "Tab1" );
-		adapter.addFragment( new FragmentText("테스트 2"), "Tab2" );
+	    viewfinderView = (ViewfinderView) findViewById(R.id.viewfinder_view);
+	    resultView = findViewById(R.id.result_view);
+	    statusView = (TextView) findViewById(R.id.status_view);
 	}
 
 	@Override
@@ -72,17 +39,6 @@ public class MainActivity extends ActionBarActivity implements MaterialTabListen
 		return super.onOptionsItemSelected(item);
 	}
 
-	/* 
-	 * 단말기 가로/세로 전환시 데이터 재 로딩 방지
-	 */
-	Configuration oldConfig = null;
-	@Override
-	public void onConfigurationChanged(Configuration newConfig) {
-		super.onConfigurationChanged(newConfig);
-	}
-	
-	
-	
 	private final int BACK_PRESSED_TIME_INTERVAL = 2000;	// 2sec
 	private long backPressedTime = 0;
 		
@@ -96,18 +52,5 @@ public class MainActivity extends ActionBarActivity implements MaterialTabListen
 			backPressedTime = System.currentTimeMillis();
 			Toast.makeText( this, R.string.two_back_touch_exit_app, Toast.LENGTH_SHORT ).show();;
 		}
-	}
-	
-	@Override
-	public void onTabSelected(MaterialTab tab) {
-		pager.setCurrentItem( tab.getPosition());
-	}
-
-	@Override
-	public void onTabReselected(MaterialTab tab) {
-	}
-
-	@Override
-	public void onTabUnselected(MaterialTab tab) {
 	}
 }
