989bb0f259d4ef8d2c12fb1e3581cbfc3040076e
purehero <purehero@gmail.com>
2017-01-23 00:45:16 +0900

    no message

--- AndroidFlashLight/src/com/purehero/flash/light/MainActivity.java
+++ AndroidFlashLight/src/com/purehero/flash/light/MainActivity.java
@@ -1,25 +1,31 @@
 package com.purehero.flash.light;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity implements OnClickListener {
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends Activity implements OnClickListener {
 
 	private FlashLight2 flashLight = null;
+	private AdView bannerAdView = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+				
 		int btnIDs [] = new int[]{ R.id.btnFlash };
 		for( int id : btnIDs ) {
 			Button btn = ( Button ) findViewById( id );
@@ -41,6 +47,22 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
             });
             alert.show();
 		}
+		
+		// 하단 배너 광고 표시
+		bannerAdView = (AdView) findViewById(R.id.adView);
+        if( bannerAdView != null ) {
+			bannerAdView.setVisibility( View.GONE );
+			
+			AdRequest adRequest = new AdRequest.Builder().build();
+			bannerAdView.setAdListener( new AdListener(){
+				@Override
+				public void onAdLoaded() {
+					super.onAdLoaded();
+					bannerAdView.setVisibility( View.VISIBLE );
+				}});
+			
+			bannerAdView.loadAd(adRequest);
+		}
 	}
 
 	@Override
@@ -61,10 +83,12 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
+		/*
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
+		*/
 		return super.onOptionsItemSelected(item);
 	}
 
@@ -82,11 +106,26 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 			flash = !flash;
 			
 			Button btn = ( Button ) arg0;
-			btn.setText( flash ? "Flash OFF" : "Flash ON" );
+			btn.setBackgroundResource( flash ? R.drawable.light_on : R.drawable.light_off );
 			
 			flashLight.setFlashLight( flash );
 			break;
-		}
+		}	
+	}
+	
+	// Back 버튼을 두번 연속으로 눌렸을때 앱을 종료하기 위해 필요한 변수 및 값
+	private final int BACK_PRESSED_TIME_INTERVAL = 2000;	// 2sec
+	private long backPressedTime = 0;
 		
+	@Override
+	public void onBackPressed() 
+	{
+		if( backPressedTime + BACK_PRESSED_TIME_INTERVAL > System.currentTimeMillis()) {
+			super.onBackPressed();
+			
+		} else {
+			backPressedTime = System.currentTimeMillis();
+			Toast.makeText( this, R.string.two_back_touch_exit_app, Toast.LENGTH_SHORT ).show();;
+		}
 	}
 }
