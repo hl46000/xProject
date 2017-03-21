79de40d947ddc842a893a1c4ea9e696afee401f9
purehero <purehero@inka.co.kr>
2017-03-21 15:23:18 +0900

    APKExtractor V1.0 출시

--- APKExtractor/ApkExtractor/src/main/java/com/purehero/apk/extractor/MainActivity.java
+++ APKExtractor/ApkExtractor/src/main/java/com/purehero/apk/extractor/MainActivity.java
@@ -61,11 +61,10 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-        */
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
+*/
 
         //
         tabHost = (MaterialTabHost) this.findViewById(R.id.tabHost);
